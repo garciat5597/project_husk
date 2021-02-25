3f4138785747400e96a60fdae7e2df9b546fca3b
TheOtherVinny <60886408+TheOtherVinny@users.noreply.github.com>
2021-02-25 15:12:04 -0600

    start of 2/25 push

--- Assets/Scripts/simpleMovement.cs
+++ Assets/Scripts/simpleMovement.cs
@@ -5,7 +5,8 @@ using UnityEngine;
 public class simpleMovement : MonoBehaviour
 {
     public float physicsMovementSpeed = 10;
-    private float translationMovementSpeed = 1;
+    public float jumpSpeedForce = 100;
+    public float translationMovementSpeed = 1;
     public Rigidbody2D playerRb;
     // Start is called before the first frame update
     void Start()
@@ -17,18 +18,38 @@ public class simpleMovement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-    // Movement using velocity
+        // Movement using velocity
+
+        if (Input.GetKeyDown(KeyCode.W) || Input.GetKeyDown(KeyCode.UpArrow))
+        {
+            playerRb.velocity = playerRb.velocity + new Vector2(0, jumpSpeedForce * 1);
+        }
+
         if (Input.GetKey(KeyCode.D)|| Input.GetKey(KeyCode.RightArrow))
         {
             playerRb.velocity = new Vector2(physicsMovementSpeed * 1, 0);
         }
 
-    // Movement using transform
+        if (Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.LeftArrow))
+        {
+            playerRb.velocity = new Vector2(physicsMovementSpeed * -1, 0);
+        }
+
+       // playerRb.velocity = new Vector2(0, -9);
+
+        /*
+        // Movement using transform
         Vector2 playPos = gameObject.transform.position;
         if (Input.GetKey(KeyCode.A)|| Input.GetKey(KeyCode.LeftArrow))
         {
             //playerRb.velocity = new Vector2(movementSpeed * -1, 0);
             gameObject.transform.position = Vector2.Lerp(playPos, playPos + Vector2.left, translationMovementSpeed / 100);
         }
+
+        if (Input.GetKey(KeyCode.D) || Input.GetKey(KeyCode.RightArrow))
+        {
+            gameObject.transform.position = Vector2.Lerp(playPos, playPos + Vector2.right, translationMovementSpeed / 100);
+        }
+        */
     }
 }
