e044f37aff66db2841eb6ee0593d00825ec09343
garciat5597 <60933658+garciat5597@users.noreply.github.com>
2021-04-28 18:52:37 -0500

    Staged assets for outtro cutscene

---
+++ Assets/Scripts/VidUrlOuttro.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Video;
+
+public class VidUrlOuttro : MonoBehaviour
+{
+    VideoPlayer video;
+    // Start is called before the first frame update
+    void Start()
+    {
+        video = GetComponent<VideoPlayer>();
+        if (video)
+        {
+            video.url = System.IO.Path.Combine(Application.streamingAssetsPath, "OuttroCutscene.mp4");
+            video.Play();
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
