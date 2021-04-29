a528e6c98513e64069b44fa7131d261cd3188825
garciat5597 <60933658+garciat5597@users.noreply.github.com>
2021-04-28 23:45:23 -0500

    THIS BOI GOT NOISE

--- Assets/Scripts/VidURLIntro.cs
+++ Assets/Scripts/VidURLIntro.cs
@@ -12,7 +12,7 @@ public class VidURLIntro : MonoBehaviour
         video = GetComponent<VideoPlayer>();
         if (video)
         {
-            video.url = System.IO.Path.Combine(Application.streamingAssetsPath, "IntroCutscene.mp4");
+            video.url = System.IO.Path.Combine(Application.streamingAssetsPath, "This boi got noise.mp4");
             video.Play();
         }
     }
